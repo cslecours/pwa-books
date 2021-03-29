cb02e15dad4d321c4674b6018a3e86b29749a1c0
Charles Simard-Lecours <charles.lecours@gmail.com>
2021-03-28 21:49:45 -0400

    move hook

--- src/App.tsx
+++ src/App.tsx
@@ -6,43 +6,6 @@ import { BookList } from './isbn/bookList';
 import { Book } from './isbn/bookModel';
 
 
-const useISBNSearch = () => {
-  const [book, setBook] = useState<Book | undefined>(undefined);
-  const [isLoading, setLoading] = useState(false);
-  const [error, setError] = useState<Error | undefined>(undefined)
-  const [valid, setValid] = useState<boolean | undefined>(undefined)
-  const [value, setValue] = useState('')
-
-  const onChange = useCallback(async (evt: React.ChangeEvent<HTMLInputElement>) => {
-    const value = evt.target.value;
-    setValue(value)
-  }, [setValue])
-
-  useEffect(() => {
-    (async () => {
-      const isValid = (value.length === 10 || value.length === 13)
-      setValid(isValid);
-      setError(undefined)
-      if (isValid) {
-        setLoading(true);
-        try {
-          const fetchedBook = await fetchBookByISBN(value);
-          setBook(fetchedBook)
-        } catch (error) {
-          setError(error)
-        }
-
-        setLoading(false)
-      } else {
-        setBook(undefined)
-      }
-    })()
-  }, [value])
-
-
-  return { onChange, book, valid, error, isLoading }
-}
-
 function App() {
   const { onChange, valid, isLoading, error, book } = useISBNSearch()
 
