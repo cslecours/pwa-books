11ef8d32cefcf5e4c432114ac5bab6b2dd9229eb
Charles Simard-Lecours <charles.lecours@gmail.com>
2021-04-09 12:33:07 -0400

    add isbn check

--- src/services/isbn.ts
+++ src/services/isbn.ts
@@ -1,5 +1,35 @@
 export function parseISBN(isbn: string) {
   const value = isbn.replace(/\D/g, '')
-  const valid = value.length === 10 || value.length === 13
-  return { valid, kind: valid ? value.length : '', value }
+  let error: 'length' | 'format13' | 'format10' | undefined = undefined
+  let valid = false
+  if (value.length === 13) {
+    const sum = Array.from(value.slice(0, 12)).reduce(
+      (acc, curr, currentIndex) => {
+        const digit = parseInt(curr, 10)
+        const multiplier = currentIndex % 2 === 0 ? 1 : 3
+        return acc + digit * multiplier
+      },
+      0
+    )
+    const remainder = sum % 10
+    if (remainder === 0) {
+      valid = true
+    } else {
+      const lastDigit = parseInt(value.slice(-1), 10)
+      valid = 10 - remainder === lastDigit
+    }
+    if (!valid) {
+      error = 'format13'
+    }
+    console.log(sum % 10, value.slice(-1))
+  } else {
+    error = 'length'
+  }
+
+  return {
+    valid,
+    kind: valid ? value.length : '',
+    value,
+    ...(error ? { error } : {}),
+  }
 }
