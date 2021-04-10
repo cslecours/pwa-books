88792fde7504278ce2b0ff67fa2f81c2af1524a9
Charles Simard-Lecours <charles.lecours@gmail.com>
2021-04-10 19:23:49 -0400

    fix isnb13 validation

--- src/services/isbn.test.ts
+++ src/services/isbn.test.ts
@@ -1,5 +1,7 @@
 import { parseISBN } from './isbn'
 
+//
+
 describe('parseIsbn10', () => {
   it.each`
     input            | output
@@ -11,6 +13,20 @@ describe('parseIsbn10', () => {
     ${'048665088X'}  | ${true}
     ${'X123456788'}  | ${false}
     ${'2222222222'}  | ${true}
+    ${'9971502100'}  | ${true}
+  `('should return $output when $input', ({ input, output }) => {
+    const { valid } = parseISBN(input)
+    expect(valid).toBe(output)
+  })
+})
+
+describe('parseIsbn13', () => {
+  it.each`
+    input                  | output
+    ${'978-1-4028-9462-6'} | ${true}
+    ${'978-1734314509'}    | ${false}
+    ${'978-1788399081'}    | ${true}
+    ${'978-1788399083'}    | ${false}
   `('should return $output when $input', ({ input, output }) => {
     const { valid } = parseISBN(input)
     expect(valid).toBe(output)
--- src/services/isbn.ts
+++ src/services/isbn.ts
@@ -1,14 +1,23 @@
+import { createImportSpecifier } from 'typescript'
+
 export function parseISBN(isbn: string) {
-  const value = isbn
+  const value = isbn.replace(/[^0-9X]/gi, '')
   let error: 'length' | 'format13' | 'format10' | undefined = undefined
   let valid = false
-  valid = value.length === 13 || value.length === 10
   if (value.length === 13) {
-    let valid = validateISBN13(value)
-    if (!valid) error = 'format13'
+    try {
+      valid = validateISBN13(value)
+      if (!valid) error = 'format13'
+    } catch {
+      error = 'format13'
+    }
   } else if (value.length === 10) {
-    let valid = validateISBN10(value)
-    if (!valid) error = 'format10'
+    try {
+      valid = validateISBN10(value)
+      if (!valid) error = 'format10'
+    } catch {
+      error = 'format10'
+    }
   } else {
     error = 'length'
   }
@@ -23,31 +32,37 @@ export function parseISBN(isbn: string) {
 
 function total(isbn: string, getMultiplier: (index: number) => number) {
   const length = isbn.length - 1
-  const total = Array.from(isbn.slice(0, length)).reduce(
-    (acc, curr, currentIndex) => {
-      const digit = parseInt(curr, 10)
-      const multiplier = getMultiplier(currentIndex)
-      return acc + digit * multiplier
-    },
-    0
-  )
+
+  const total = Array.from(isbn.slice(0, length))
+    .map((char, index) => {
+      const digit = parseInt(char, 10)
+      if (Number.isNaN(digit)) {
+        throw new Error('Invalid Format')
+      }
+      const multiplier = getMultiplier(index)
+      return digit * multiplier
+    })
+    .reduce((acc, curr) => acc + curr)
+
   return total
 }
 
 export function validateISBN10(isbn: string): boolean {
   const lastChar = isbn.slice(-1)
   const check = lastChar === 'X' ? 10 : parseInt(lastChar)
-  return total(isbn, (idx) => idx + 1) % 11 === check
+  const sum = total(isbn, (idx) => idx + 1)
+  return sum % 11 === check
 }
 
 function validateISBN13(isbn: string): boolean {
   let valid = false
-  const remainder = total(isbn, (idx) => (idx % 2 ? 1 : 3))
+  const remainder = total(isbn, (idx) => (idx % 2 ? 1 : 3)) % 10
+  const checkDigit = parseInt(isbn.slice(-1), 10)
   if (remainder === 0) {
-    valid = true
+    valid = checkDigit === 0
   } else {
-    const lastDigit = parseInt(isbn.slice(-1), 10)
-    valid = 10 - remainder === lastDigit
+    const restOfRemainder = 10 - remainder
+    valid = restOfRemainder === checkDigit || remainder == checkDigit
   }
   return valid
 }
